8b6d4c44588d05897e2c39c525da3f6cbe162ec7
Andrew Ziegler <Andrew.Ziegler@dominodatalab.com>
2020-04-22 13:28:48 -0400

    modified make

--- make.sh
+++ make.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-for i in {1..10}
+for i in {1..100}
 do
    echo "Building $i commit"
-   head -c 100000000 /dev/urandom > big1
+   head -c 1000000 /dev/urandom > small
    git add .
    git commit -m "asdf"
    git push origin master
