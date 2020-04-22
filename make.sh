1c202fc5036b2a73d6c422c7d571dfb74aa66d7b
Andrew Ziegler <Andrew.Ziegler@dominodatalab.com>
2020-04-22 13:18:58 -0400

    asdf

--- make.sh
+++ make.sh
@@ -5,10 +5,6 @@ for i in {1..10}
 do
    echo "Building $i commit"
    head -c 100000000 /dev/urandom > big1
-   head -c 100000000 /dev/urandom > big2
-   head -c 100000000 /dev/urandom > big3
-   head -c 100000000 /dev/urandom > big5
-   head -c 100000000 /dev/urandom > big6
    git add .
    git commit -m "asdf"
    git push origin master
